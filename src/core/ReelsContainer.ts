c3bc1bb116e75be66e6620da786b1c0eed559178
unige <unigenocity@gmail.com>
2020-09-16 18:00:46 +0300

    Encapsulated redundant logic from Game class

--- src/core/ReelsContainer.ts
+++ src/core/ReelsContainer.ts
@@ -18,7 +18,40 @@ export default class ReelsContainer {
         this.container.x = REEL_OFFSET_LEFT;
     }
 
-    blessRNG() {
+    async spin() {
+        const start = Date.now();
+        for await (let value of this.infiniteSpinning()) {
+            if (Date.now() >= start + 2000) break;
+        }
+
+        const arr = this.reels.map(reel => reel.sprites[1]);
+        return this.checkIfWin(arr);
+    }
+
+    private async* infiniteSpinning() {
+        while (true) {
+            for (let i = 0; i < this.reels.length; i++) {
+                if (i === this.reels.length - 1) {
+                    await this.reels[i].spinOneTime();
+                    this.blessRNG();
+                    yield;
+                }
+
+                this.reels[i].spinOneTime();
+            }
+        }
+    }
+
+    private checkIfWin(symbols: Array<PIXI.Sprite>): boolean {
+        // Set of strings: 'SYM1', 'SYM2', ...
+        //
+        const combination: Set<string> = new Set();
+        symbols.forEach(symbol => combination.add(symbol.texture.textureCacheIds[0].split('.')[0]));
+        if (combination.size === 1 && !combination.has('SYM1')) return true;
+        return combination.size === 2 && combination.has('SYM1');
+    }
+
+    private blessRNG() {
         this.reels.forEach(reel => {
             reel.sprites[0].texture = reel.textures[Math.floor(Math.random() * reel.textures.length)];
         });
