322370ff11db194d55b6b0c2f44b75937c5012c8
Oleksandr Siryk <Oleksandr.Siryk@netent.com>
2021-07-23 00:55:32 +0300

    Remove express. Implement pure nodejs static server.

--- server.ts
+++ server.ts
@@ -1,16 +1,18 @@
-import express, { Request, Response } from 'express';
-import path from 'path';
-
-const app = express();
-
-app.use(express.static('./build'));
-
-app.get('*', (req: Request, res: Response) => {
-    res.sendFile(path.resolve('./build/index.html'));
-});
+import { join } from "path";
+import { promises as fs } from "fs";
+import { createServer } from "http";
 
 const PORT = process.env.PORT || 8080;
+const STATIC_PATH = join("./", "build");
 
-app.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`);
-});
+createServer(async (req, res) => {
+    const url = req.url === "/" ? "/index.html" : `${req.url}`;
+    const filePath = join(STATIC_PATH, url);
+    try {
+        const data = await fs.readFile(filePath);
+        res.end(data);
+    } catch (err) {
+        res.statusCode = 404;
+        res.end(`File "${url}" is not found`);
+    }
+}).listen(PORT, () => console.log(`Static on port ${PORT}`));
